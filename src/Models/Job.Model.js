626b5469aa56149a037c2e5c3132c7d69b5f2704
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-22 09:33:09 +0600

    d

---
+++ src/Models/Job.Model.js
@@ -0,0 +1,157 @@
+import { model, Schema } from "mongoose";
+
+const jobSchema = Schema(
+  {
+    heading: {
+      type: String,
+      required: true,
+      minlength: [5, `The heading must be at least 5 characters long.`],
+      maxlength: [100, `The heading must not be more then 100 characters.`],
+    },
+    title: {
+        type: String,
+        required: true,
+        minlength: [5, `The title must be at least 5 characters long.`],
+        maxlength: [400, `The title must not be more then 100 characters.`],
+      },
+      description: {
+        type: String,
+        required: true,
+        minlength: [5, `The description must be at least 5 characters long.`],
+        maxlength: [2000, `The description must not be more then 100 characters.`],
+      },
+  
+      requirments: {
+        type: String,
+        required: true,
+        minlength: [5, `The description must be at least 5 characters long.`],
+        maxlength: [2000, `The description must not be more then 100 characters.`],
+      },
+      howtoapply:{
+        type: String,
+        required: true,
+        minlength: [5, `The description must be at least 5 characters long.`],
+        maxlength: [2000, `The description must not be more then 100 characters.`],
+      },
+      qualifications: {
+        type: String,
+        required: true,
+        minlength: [5, `The description must be at least 5 characters long.`],
+        maxlength: [2000, `The description must not be more then 100 characters.`],
+      },
+      Workplace: {
+      type: String,
+      required: true,
+      default: "yearly",
+      enum: ["onsite", "remote", "hybride", "negotiable"]
+      // enum: ["yearly", "monthly", "weekly", "negotiable", "never"]
+    },
+    salaryreview: {
+      type: String,
+      required: true,
+      default: "yearly",
+      enum: ["yearly", "monthly", "weekly", "negotiable", "never"]
+    },
+    festivalbonus:{
+      type: String,
+      required: true,
+      default: "yearly",
+      enum: ["yearly", "monthly", "weekly", "negotiable", "never"]
+    },
+    Salary:{
+      type: Number,
+      required: true,
+      default: 0
+    },
+    Deadline: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return v > Date.now();
+        },
+        message: props => `Deadline (${props.value}) cannot be in the past!`
+      }
+    },
+    address:{
+      type: String,
+      required: true,
+      minlength: [5, `The address must be at least 5 characters long.`],
+      maxlength: [2000, `The address must not be more then 100 characters.`],
+    },
+    companyname:{
+      type: String,
+      required: true,
+      minlength: [5, `The company name must be at least 5 characters long.`],
+      maxlength: [100, `The company name must not be more then 100 characters.`],
+    },
+    companynabout:{
+      type: String,
+      required: true,
+      minlength: [5, `The company about must be at least 5 characters long.`],
+      maxlength: [2000, `The company about must not be more then 100 characters.`],
+    },
+    companylogo:{
+      type: String,
+      required: true,
+    },
+    companylinkedin:{
+      type: String,
+      required: true,
+    },
+    companyemail:{
+      type: String,
+      required: true,
+    },
+    azpectedareafrom:{
+      type: String,
+      required: true,
+      minlength: [5, `The azpected area from must be at least 5 characters long.`],
+      maxlength: [2000, `The azpected area from must not be more then 100 characters.`],
+    },
+    aditionalskill:{
+      type: String,
+      required: true,
+      minlength: [5, `The aditional skill from must be at least 5 characters long.`],
+      maxlength: [2000, `The aditional skill from must not be more then 100 characters.`],
+    },
+    benefits:{
+      type: String,
+      required: true,
+      minlength: [5, `The benefits from must be at least 5 characters long.`],
+      maxlength: [2000, `The benefits from must not be more then 100 characters.`],
+    },
+    employmentstatus:{
+      type: String,
+      required: true,
+      default: "fulltime",
+      enum: ["fulltime", "parttime", "negotiable"]
+    },
+    skills:{
+      type: Array,
+      required: true,
+      default: [],
+    },
+    cetagory:{
+      type: Array,
+      required: true,
+      default: [],
+    },
+    apllication:{
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    apllicatorids:{
+      type: Array,
+      required: false,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Job = model("job", jobSchema);
+export default Job;
--- src/app/dashboard/managejobs/page.jsx
+++ src/app/dashboard/managejobs/page.jsx
@@ -15,35 +15,35 @@ export default function page() {
         <div className="space-y-4 p-2 lg:p-10 lg:pt-5 text-center">
         <div className=" text-right">
           <button
-            className="p-4 px-20 loading rounded-xl bg-slate-800 text-white"
+            className="p-4 px-20 loading rounded-xl bg-slate-200 text-white"
           >
           </button>
         </div>
 
-        <div className="p-4 lg:px-8 loadingbig bg-slate-900 text-slate-300 flex items-center">
+        <div className="p-4 lg:px-8 loadingbig bg-slate-200 text-slate-300 flex items-center">
         </div>
 
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
-        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-900 text-white flex items-center">
+        <div className="p-4 lg:px-8 loadingbig hover:bg-slate-200 text-white flex items-center">
         </div>
       </div>
     );
@@ -73,11 +73,11 @@ export default function page() {
       <table className="min-w-full bg-slate-900 text-slate-300">
         <thead>
           <tr>
-            <th className="lg:text-lg text-xs w-3/12 px-2 py-2">Name</th>
-            <th className="lg:text-lg text-xs w-3/12 px-2 py-2">Email</th>
-            <th className="lg:text-lg text-xs w-2/12 px-2 py-2">User type</th>
-            <th className="lg:text-lg text-xs w-2/12 px-2 py-2">Country</th>
-            <th className="lg:text-lg text-xs w-2/12 px-2 py-2">Status</th>
+            <th className="lg:text-lg text-xs w-3/12 px-2 py-2">Job Deating</th>
+            <th className="lg:text-lg text-xs w-3/12 px-2 py-2">Posted Date</th>
+            <th className="lg:text-lg text-xs w-2/12 px-2 py-2">Deadline</th>
+            <th className="lg:text-lg text-xs w-2/12 px-2 py-2">Applied Candidate</th>
+            <th className="lg:text-lg text-xs w-2/12 px-2 py-2">Short listed CV</th>
           </tr>
         </thead>
         <tbody>
