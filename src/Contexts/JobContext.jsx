7447579fb8529c9bf29bcdb19e485a31601e02c0
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-22 09:45:06 +0600

    d

--- src/Contexts/JobContext.jsx
+++ src/Contexts/JobContext.jsx
@@ -3,6 +3,10 @@ const { createContext, useReducer } = require("react");
 
 export const JobContext = createContext()
 
+const jobReducer = () => {
+    console.log("hello")
+}
+
 export const JobContextProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(jobReducer, {
