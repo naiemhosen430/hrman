43a98652c202f43510625895c80c330c6b168df4
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-23 12:58:55 +0600

    d

--- src/app/Components/Cards/JobCard.jsx
+++ src/app/Components/Cards/JobCard.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 export default function JobCard({data}) {
   return (
     <>
-        <div className="lg:p-4 p-2 rounded-md border border-red-500">
+        <div className="lg:p-4 p-2 minh-[300px] rounded-md border border-red-500">
             <div className="flex justify-between">
-                <div className="border inline-block">
+                <div className="border inline-block p-2">
                 <img className="h-[50px] w-[50px] opacity-60" src={data?.companylogo} alt="" />
                 </div>
                 <div className="inline-block">
@@ -14,11 +14,11 @@ export default function JobCard({data}) {
                 </div>
             </div>
 
-            <h1 className="lg:text-xl font-bold">{data?.heading}</h1>
+            <h1 className="lg:text-lg py-2 font-bold">{data?.heading?.slice(0,25)}</h1>
 
             <div className="flex items-center text-xs">
-                <span className="bg-rose-300 rounded-md text-white inline-block mr-2 p-1 px-2">{data?.workplace}</span>
-                <span className="rounded-md inline-block mr-2 p-1 px-2">{data?.address}</span>
+                <span className="bg-rose-300 rounded-md text-white inline-block p-1 px-2">{data?.workplace}</span>
+                <span className="rounded-md inline-block p-1 px-2">{data?.address?.slice(0,20)}</span>
             </div>
 
             <span className="block rounded-md text-xs mr-2 py-1">Type: {data?.employmentstatus}</span>
@@ -26,7 +26,7 @@ export default function JobCard({data}) {
             <span className="block rounded-md text-xs mr-2 py-1">Selary: {data?.salary}/=</span>
 
 
-            <p className="block rounded-md text-xs mr-2 py-1">{data?.title}</p>
+            <p className="block rounded-md text-xs mr-2 py-1">{data?.title?.slice(0,150)}</p>
         </div>
     </>
   )
