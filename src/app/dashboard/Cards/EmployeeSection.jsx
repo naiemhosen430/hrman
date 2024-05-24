9d2f8c64cdd0b95210234252619e6a96e52884f3
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-24 11:50:44 +0600

    d

--- src/app/dashboard/Cards/EmployeeSection.jsx
+++ src/app/dashboard/Cards/EmployeeSection.jsx
@@ -5,18 +5,51 @@ import { GrLinkNext } from "react-icons/gr";
 export default function EmployeeSection() {
   return (
     <>
-        <div className="flex items-center py-10">
+        <div className="flex py-10">
 
-            <div className="rounded-l-5 border lg:w-2/12 p-4">
-                <MdDoNotDisturbOnTotalSilence className="text-6xl font-bold" />
+            <div className="rounded-l-5 border lg:w-[20%] p-4">
+                <MdDoNotDisturbOnTotalSilence className="text-6xl mb-5 font-bold" />
 
-                <h1 classname="text-2xl py-5">New Employee</h1>
+                <h1 className="text-xl py-5">New Employee</h1>
 
                 <div className="flex items-center">
-                  <h1 classname="text-4xl font-bold py-5">450</h1>
-                  <h1 classname="text-xl bg-[#D9D9D9] px-4 rounded-full flex items-center font-bold py-5"><GrLinkNext /> 0.0%</h1>
+                  <h1 className="text-4xl  font-bold ">450</h1>
+                  <h1 className="text-sm mx-2 text-[#09D070] bg-[#D9D9D9] px-4 py-1 rounded-full flex items-center font-bold "><GrLinkNext className="inline-block" /> <span className="inline-block">0.0%</span></h1>
                 </div>
 
+            </div>
+
+            <div className="rounded-l-5 border lg:w-[20%] p-4">
+                <MdDoNotDisturbOnTotalSilence className="text-6xl mb-5 font-bold" />
+
+                <h1 className="text-xl py-5">New Employee</h1>
+
+                <div className="flex items-center">
+                  <h1 className="text-4xl  font-bold ">450</h1>
+                  <h1 className="text-sm mx-2 text-[#09D070] bg-[#D9D9D9] px-4 py-1 rounded-full flex items-center font-bold "><GrLinkNext className="inline-block" /> <span className="inline-block">0.0%</span></h1>
+                </div>
+
+            </div>
+
+            <div className="rounded-l-5 border lg:w-[20%] p-4">
+                <MdDoNotDisturbOnTotalSilence className="text-6xl mb-5 font-bold" />
+
+                <h1 className="text-xl py-5">Resigned Employee</h1>
+
+                <div className="flex items-center">
+                  <h1 className="text-4xl  font-bold ">450</h1>
+                  <h1 className="text-sm mx-2 text-[#09D070] bg-[#D9D9D9] px-4 py-1 rounded-full flex items-center font-bold "><GrLinkNext className="inline-block" /> <span className="inline-block">0.0%</span></h1>
+                </div>
+
+            </div>
+
+            <div className="rounded-l-5 border lg:w-[20%] p-0">
+                  <h1 className="text-[17px] p-4 px-2 border-b py-5">Employee Demographics</h1>
+
+                  <div className="flex items-center">
+                    <h1 className="text-4xl  font-bold ">450</h1>
+                    <h1 className="text-sm mx-2 text-[#09D070] bg-[#D9D9D9] px-4 py-1 rounded-full flex items-center font-bold "><GrLinkNext className="inline-block" /> <span className="inline-block">0.0%</span></h1>
+                  </div>
             </div>
 
         </div>
