de25d72ecc3448849242073b3edb7d46b9b2ab67
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-24 09:31:25 +0600

    d

--- src/Utils/hundleError.js
+++ src/Utils/hundleError.js
@@ -1,7 +1,11 @@
 import { toast } from "react-toastify";
+import { setCookie } from "cookies-next";
 
 export const handleError = (error) => {
     if (error.response) {
+        if (error?.response?.status === 401){
+            setCookie("accesstoken", null);
+        }
         toast.error(`Server Error: ${error.response.data.message || 'An error occurred'}`);
     } else if (error.request) {
         toast.error('Error: No response received from the server');
